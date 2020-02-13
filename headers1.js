a6c151239395af3e064f9897b31a7ecf16e02fc3
andy.rothwell <rothwell.andy@gmail.com>
2020-02-13 10:45:34 -0500

    first commit

---
+++ headers1.js
@@ -0,0 +1,26 @@
+Request URL: https://basemaps.arcgis.com/v1/arcgis/rest/services/World_Basemap/VectorTileServer/tile/14/6207/4774.pbf
+Request Method: GET
+Status Code: 200  (from disk cache)
+Remote Address: 192.168.40.243:8080
+Referrer Policy: no-referrer-when-downgrade
+accept-ranges: bytes
+access-control-allow-methods: GET, HEAD
+access-control-allow-origin: *
+cache-control: max-age=3600
+content-disposition: attachment; filename="4774.pbf"
+content-length: 9418
+content-type: binary/octet-stream
+date: Mon, 13 Jan 2020 21:57:54 GMT
+etag: "ccacd3f10fca1a0d3266575e1cc2e750"
+last-modified: Fri, 17 Nov 2017 04:07:44 GMT
+server: AmazonS3
+status: 200
+vary: Origin,Access-Control-Request-Headers,Access-Control-Request-Method
+via: 1.1 95e9f369e54eb77c8abfd772b46c5a7b.cloudfront.net (CloudFront)
+x-amz-cf-id: 5fzsKChfWVa1N2iRkcyMn-cZD7_NHyYWFa3fQOQ6NCJdo7DuxBDXng==
+x-amz-cf-pop: EWR52-C4
+x-cache: Miss from cloudfront
+Provisional headers are shown
+Origin: http://10.8.102.67:8080
+Referer: http://10.8.102.67:8080/index.html
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
---
+++ headers2.js
@@ -0,0 +1,27 @@
+Request URL: https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/PVL_Original/VectorTileServer/tile/14/6205/4770.pbf
+Request Method: GET
+Status Code: 200  (from disk cache)
+Remote Address: 192.168.40.243:8080
+Referrer Policy: no-referrer-when-downgrade
+access-control-allow-origin: http://10.8.102.67:8080
+cache-control: no-transform, max-age=3600, s-maxage=3600
+content-length: 0
+content-type: application/octet-stream
+date: Mon, 13 Jan 2020 21:53:21 GMT
+etag: "c7dcb9cf6a73111c27d2e2d412013b31"
+last-modified: Thu, 05 Dec 2019 17:47:13 GMT
+status: 200
+vary: Origin,Accept-Encoding,Authorization,X-Esri-Authorization
+via: 1.1 d8414eaa760a9f2eb470f4b13ca82cb7.cloudfront.net (CloudFront)
+x-amz-cf-id: ghO844Ob1QLDNTRQNup4YG9waBDKcyasKmjufmmn2TQLA0fTVggBjA==
+x-amz-cf-pop: EWR52-C2
+x-arcgis-info: resample=true; clip=true; 14,6205,4770; 2,1,1; time=482ms
+x-arcgis-instance-id: i-0ffe1834b13b63429
+x-cache: Miss from cloudfront
+x-content-type-options: nosniff
+x-powered-by: ArcGIS-Online-Hosted-Services
+x-xss-protection: 1; mode=block
+Provisional headers are shown
+Origin: http://10.8.102.67:8080
+Referer: http://10.8.102.67:8080/index.html
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
